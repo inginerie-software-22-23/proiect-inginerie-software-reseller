1d7da221a2c908997cb039029ac8f6109866ed97
Octavian Mitrica <octavian.mitrica4@gmail.com>
2022-12-11 20:19:55 +0200

    Generate project

---
+++ Reseller/src/main/java/com/m15/Reseller/ResellerApplication.java
@@ -0,0 +1,13 @@
+package com.m15.Reseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellerApplication.class, args);
+	}
+
+}
