7689184c15f8f6dba0b5aa862e4e244be0756af5
Octavian Mitrica <octavian.mitrica4@gmail.com>
2022-12-13 10:19:47 +0200

    Created models

---
+++ Reseller/src/main/java/com/m15/Reseller/model/User.java
@@ -0,0 +1,33 @@
+package com.m15.Reseller.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    @NotBlank(message = "Username is required")
+    private String username;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    private Instant created;
+    private boolean enabled;
+}
---
+++ Reseller/src/main/java/com/m15/Reseller/model/VerificationToken.java
@@ -0,0 +1,19 @@
+package com.m15.Reseller.model;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "token")
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+    private Instant expiryDate;
+}
