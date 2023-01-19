d5fac859409c9780a193ed4c5a54747e8bb1bafe
Stancu Denisa <denisastancu354@gmail.com>
2023-01-19 16:40:01 +0200

    added my profile page and fixed comments

--- Reseller-Frontend/src/app/signup/signup.component.ts
+++ Reseller-Frontend/src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SignupRequestPayload } from '../models/signup-request.payload';
 import { AuthService } from '../sevices/auth.service';
 
@@ -13,9 +14,10 @@ export class SignupComponent implements OnInit {
   signupForm!: FormGroup;
   signupRequestPayload!: SignupRequestPayload;
 
-  constructor(private authService: AuthService) { 
+  constructor(private authService: AuthService, private router: Router) { 
     this.signupRequestPayload = {
       username: '',
+      fullname: '',
       email: '',
       password: ''
     };
@@ -24,6 +26,7 @@ export class SignupComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       username: new FormControl('', Validators.required),
+      fullname: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required)
     });
@@ -32,11 +35,13 @@ export class SignupComponent implements OnInit {
   signup() {
 
     this.signupRequestPayload.username = this.signupForm.get('username')?.value;
+    this.signupRequestPayload.username = this.signupForm.get('fullname')?.value;
     this.signupRequestPayload.email = this.signupForm.get('email')?.value;
     this.signupRequestPayload.password = this.signupForm.get('password')?.value;
 
     this.authService.signup(this.signupRequestPayload).subscribe(() => {
       console.log('Signup Successful');
+      this.router.navigate(['/home']);
     }, () => {
       console.log('Signup Failed');
     });
